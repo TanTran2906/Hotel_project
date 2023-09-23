503f76b4468c8341840291f8d20fc9d29ed9437a
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2023-09-23 23:24:11 +0700

    Form for cabin

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,24 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+    return (
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All cabins</Heading>
+            </Row>
+
+            <Row>
+                <CabinTable />
+            </Row>
+
+            <Row>
+                <CreateCabinForm />
+            </Row>
+        </>
+    );
 }
 
 export default Cabins;
